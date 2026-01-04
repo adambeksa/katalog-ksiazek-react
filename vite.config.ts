29aa375bfd6806ec2d426bb10973e6739e56f96e
Adam Beksa <adam.beksa@euro.com.pl>
2026-01-04 19:57:14 +0100

    feat(eui): added mixins and variables to scss

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "/src/styles/_variables.scss" as *;
+          @use "/src/styles/_mixins.scss" as *;
+        `
+      }
+    }
+  }
 })
 
 
